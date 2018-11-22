c1b0b069b2ad7edbf76564ccbb0fc26f8476bb89
mhd-zulqarnain <zulqarnain.redcode@outlook.com>
2018-11-22 15:20:11 +0500

    free domain publishing

--- ApiSleepingPatener/Controllers/Network/MakeTableController.cs
+++ ApiSleepingPatener/Controllers/Network/MakeTableController.cs
@@ -546,11 +546,11 @@ namespace ApiSleepingPatener.Controllers
                     //var usercheckCNIC = dc.NewUserRegistrations.Where(a => a.CNIC.Equals(model.CNICNumber)).FirstOrDefault();
                     if (usercheckEmail != null)
                     {
-                        return Ok(new { error = true, message = "User email already exist" });
+                        return Ok(new { success = false, message = "User email already exist" });
                     }
                     else if (usercheckPhone != null)
                     {
-                        return Ok(new { error = true, message = "User phone number already exist" });
+                        return Ok(new { success = false, message = "User phone number already exist" });
                     }
                     //else if (usercheckAccountNumber != null)
                     //{
@@ -681,26 +681,26 @@ namespace ApiSleepingPatener.Controllers
 
                         #region user email
 
-                        System.Net.Mail.MailMessage mail1 = new System.Net.Mail.MailMessage();
-                        mail1.From = new MailAddress("noreply@sleepingpartnermanagementportalrct.com");
-                        mail1.To.Add(model.Email);
-                        mail1.Subject = "Sleeping partner management portal";
-                        mail1.Body = "User accept by admin. " +
-                            " Your Username is " + model.Username + " and password : " + model.Password + "</br></br>" +
-                            "<table style='font-family:Verdana, Helvetica, sans-serif;' cellpadding='0' cellspacing='0'><tbody><tr><td style='font-family:Verdana; border-right:2px solid #BD272D; padding-right:15px; text-align: right; vertical-align:top; ' valign='top'><table style='font-family:Verdana; margin-right:0; margin-left:auto;' cellpadding='0' cellspacing='0'><tbody><tr><td style='font-family:Verdana; height:55px; vertical-align:top; text-align:right;' valign='top' align='right'><span style='font-family:Verdana; font-size:14pt; font-weight:bold'>Sleeping partner management<span><br></span></span></td></tr><tr><td style='font-family:Verdana; height:40px; vertical-align:top; padding:0; text-align:right;' valign='top' align='right'><span style='font-family:Verdana; font-size:10pt;'>phone: 123456<span><br></span></span><span style='font-family:Verdana; font-size:10pt;'>mobile: 0123456</span></td></tr><tr><td><a href='http://sleepingpartnermanagementportalrct.com'>sleepingpartnermanagementportal</a></td></tr></tbody></table></td><td style='padding-left:15px;font-size:1pt; vertical-align:top; font-family:Verdana;' valign='top'><table style='font-family:Verdana;' cellpadding='0' cellspacing='0'><tbody><tr><td style='height:55px; font-family:Verdana; vertical-align:top;' valign='top'><a href='{Logo URL}' target='_blank'><img alt='Logo' style='height:40px; width:auto; border:0; ' height='40' border='0'  src='~/Content/images/newsleepinglogo.png'></a></td></tr><tr><td style='height:40px; font-family:Verdana; vertical-align:top; padding:0;' valign='top'><span style='font-family:Verdana; font-size:10pt;'>{Address 1}<span><br></span></span> <span style='font-family:Verdana; font-size:10pt;'>{Address 2}</span> </td></tr><tr><td style='height:20px; font-family:Verdana; vertical-align:middle;' valign='middle'><a href='http://{Web page}' target='_blank' style='color:#BD272D; font-size:10pt; font-family:Verdana;'>{Web page}</a></td></tr></tbody></table></td></tr></tbody></table>";
-                        mail1.IsBodyHtml = true;
-                        SmtpClient smtp1 = new SmtpClient();
-                        smtp1.Host = "sleepingpartnermanagementportalrct.com";
-                        smtp1.EnableSsl = true;
-                        smtp1.UseDefaultCredentials = false;
-                        smtp1.Credentials = new NetworkCredential("noreply@sleepingpartnermanagementportalrct.com", "Yly21#p8");
-                        smtp1.DeliveryMethod = SmtpDeliveryMethod.Network;
-                        smtp1.Port = 25;
-                        ServicePointManager.ServerCertificateValidationCallback =
-                        delegate (object s, X509Certificate certificate,
-                                 X509Chain chain, SslPolicyErrors sslPolicyErrors)
-                        { return true; };
-                        smtp1.Send(mail1);
+                       // System.Net.Mail.MailMessage mail1 = new System.Net.Mail.MailMessage();
+                        //mail1.From = new MailAddress("noreply@sleepingpartnermanagementportalrct.com");
+                        //mail1.To.Add(model.Email);
+                        //mail1.Subject = "Sleeping partner management portal";
+                        //mail1.Body = "User accept by admin. " +
+                        //    " Your Username is " + model.Username + " and password : " + model.Password + "</br></br>" +
+                        //    "<table style='font-family:Verdana, Helvetica, sans-serif;' cellpadding='0' cellspacing='0'><tbody><tr><td style='font-family:Verdana; border-right:2px solid #BD272D; padding-right:15px; text-align: right; vertical-align:top; ' valign='top'><table style='font-family:Verdana; margin-right:0; margin-left:auto;' cellpadding='0' cellspacing='0'><tbody><tr><td style='font-family:Verdana; height:55px; vertical-align:top; text-align:right;' valign='top' align='right'><span style='font-family:Verdana; font-size:14pt; font-weight:bold'>Sleeping partner management<span><br></span></span></td></tr><tr><td style='font-family:Verdana; height:40px; vertical-align:top; padding:0; text-align:right;' valign='top' align='right'><span style='font-family:Verdana; font-size:10pt;'>phone: 123456<span><br></span></span><span style='font-family:Verdana; font-size:10pt;'>mobile: 0123456</span></td></tr><tr><td><a href='http://sleepingpartnermanagementportalrct.com'>sleepingpartnermanagementportal</a></td></tr></tbody></table></td><td style='padding-left:15px;font-size:1pt; vertical-align:top; font-family:Verdana;' valign='top'><table style='font-family:Verdana;' cellpadding='0' cellspacing='0'><tbody><tr><td style='height:55px; font-family:Verdana; vertical-align:top;' valign='top'><a href='{Logo URL}' target='_blank'><img alt='Logo' style='height:40px; width:auto; border:0; ' height='40' border='0'  src='~/Content/images/newsleepinglogo.png'></a></td></tr><tr><td style='height:40px; font-family:Verdana; vertical-align:top; padding:0;' valign='top'><span style='font-family:Verdana; font-size:10pt;'>{Address 1}<span><br></span></span> <span style='font-family:Verdana; font-size:10pt;'>{Address 2}</span> </td></tr><tr><td style='height:20px; font-family:Verdana; vertical-align:middle;' valign='middle'><a href='http://{Web page}' target='_blank' style='color:#BD272D; font-size:10pt; font-family:Verdana;'>{Web page}</a></td></tr></tbody></table></td></tr></tbody></table>";
+                        //mail1.IsBodyHtml = true;
+                        //SmtpClient smtp1 = new SmtpClient();
+                        //smtp1.Host = "sleepingpartnermanagementportalrct.com";
+                        //smtp1.EnableSsl = true;
+                        //smtp1.UseDefaultCredentials = false;
+                        //smtp1.Credentials = new NetworkCredential("noreply@sleepingpartnermanagementportalrct.com", "Yly21#p8");
+                        //smtp1.DeliveryMethod = SmtpDeliveryMethod.Network;
+                        //smtp1.Port = 25;
+                        //ServicePointManager.ServerCertificateValidationCallback =
+                        //delegate (object s, X509Certificate certificate,
+                        //         X509Chain chain, SslPolicyErrors sslPolicyErrors)
+                        //{ return true; };
+                        //smtp1.Send(mail1);
                         //await SendEmailToSponsor(newuserdata.Email, "sleeping patners", Body);
 
                         #endregion
@@ -715,7 +715,7 @@ namespace ApiSleepingPatener.Controllers
             }
             catch (Exception ex)
             {
-                return Ok(new { error = true, message = ex.Message });
+                return Ok(new { success = false, message = ex.Message });
             }
 
         }
@@ -735,11 +735,11 @@ namespace ApiSleepingPatener.Controllers
                     //var usercheckCNIC = dc.NewUserRegistrations.Where(a => a.CNIC.Equals(model.CNICNumber)).FirstOrDefault();
                     if (usercheckEmail != null)
                     {
-                        return Ok(new { error = true, message = "User email already exist" });
+                        return Ok(new { success = false, message = "User email already exist" });
                     }
                     else if (usercheckPhone != null)
                     {
-                        return Ok(new { error = true, message = "User phone number already exist" });
+                        return Ok(new { success = false, message = "User phone number already exist" });
                     }
                     //else if (usercheckAccountNumber != null)
                     //{
@@ -906,7 +906,7 @@ namespace ApiSleepingPatener.Controllers
             }
             catch (Exception ex)
             {
-                return Ok(new { error = true, message = ex.Message });
+                return Ok(new { success = false, message = ex.Message });
             }
 
         }
