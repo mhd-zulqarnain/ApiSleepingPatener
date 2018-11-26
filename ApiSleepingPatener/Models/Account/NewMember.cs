17990ebab3235df9db29e8bbb81fceab7d2b14c7
Tuaha <tuaha@gmail.com>
2018-11-26 17:58:24 +0500

    allcontrolleradded

---
+++ ApiSleepingPatener/Models/Account/NewMember.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiSleepingPatener.Models.Account
+{
+    public class NewMember
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public int? Country { get; set; }
+        public string Phone { get; set; }
+        public string AccountNumber { get; set; }
+        public string BankName { get; set; }
+        public int? SponsorId { get; set; }
+        public string SponsorName { get; set; }
+        public decimal? PaidAmount { get; set; }
+        public int UserPackagePrice { get; set; }
+        public decimal EWalletBalance { get; set; }
+    }
+}
