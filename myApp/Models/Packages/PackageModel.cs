277eab2612ec4249b05417cee186c28c2e0f2183
mhd-zulqarnain <zulqarnain.redcode@outlook.com>
2018-12-10 17:33:56 +0500

    publishing

---
+++ myApp/Models/Packages/PackageModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ApiSleepingPatener.Models.Packages
+{
+    public class PackageModel
+    {
+        public int PackageId { get; set; }
+        [Required(ErrorMessage = "enter package name")]
+        public string PackageName { get; set; }
+        [Required(ErrorMessage = "enter package percent")]
+        public int? PackagePercent { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public DateTime CreateDate { get; set; }
+        [Required(ErrorMessage = "enter package price")]        
+        public decimal? PackagePrice { get; set; }
+        [Required(ErrorMessage = "enter package validity")]        
+        public string PackageValidity { get; set; }
+        [Required(ErrorMessage = "enter package minimum withdrawal amount")]
+        public decimal? PackageMinWithdrawalAmount { get; set; }
+        [Required(ErrorMessage = "enter package maximum withdrawal amount")]
+        public decimal? PackageMaxWithdrawalAmount { get; set; }
+        [Required(ErrorMessage = "enter member list")]
+        public int? AddMemberLimit { get; set; }
+        [Required(ErrorMessage = "enter amount")]
+        public decimal? MaximumMatchingBonusAmount { get; set; }
+    }
+}
