ec51f6a87a88c092c196defe54fc619887c08a53
zeeq <z@g.com>
2018-12-27 16:19:44 +0500

    bug solved withdrawal request remaining

---
+++ ApiSleepingPatener/Models/DetailModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ApiSleepingPatener.Models
+{
+ 
+     public partial class DetailModel
+    {
+        public int TransactionId { get; set; }
+        public string TransactionSource { get; set; }
+        public string TransactionName { get; set; }
+        public Nullable<int> AsscociatedMember { get; set; }
+        public Nullable<decimal> Amount { get; set; }
+        public Nullable<System.DateTime> TransactionDate { get; set; }
+        public Nullable<bool> Credit { get; set; }
+        public Nullable<bool> Debit { get; set; }
+        public Nullable<int> UserId { get; set; }
+        public Nullable<bool> IsPackageBonus { get; set; }
+        public Nullable<int> PackageId { get; set; }
+        public Nullable<bool> IsMatchingBonus { get; set; }
+        public Nullable<bool> IsParentBonus { get; set; }
+        public Nullable<bool> IsWithdrawlRequestByUser { get; set; }
+        public Nullable<bool> IsWithdrawlPaidByAdmin { get; set; }
+        public Nullable<bool> AdminCredit { get; set; }
+        public Nullable<bool> AdminDebit { get; set; }
+        public string AdminTransactionName { get; set; }
+        public Nullable<bool> IsWithdrawlRequestApproved { get; set; }
+    }
+}
