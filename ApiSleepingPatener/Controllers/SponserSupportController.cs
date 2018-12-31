bb67daa6097ae854a33b24ce34865022292ccb8e
zeeq <z@g.com>
2018-12-31 17:25:20 +0500

    live database

--- ApiSleepingPatener/Controllers/SponserSupportController.cs
+++ ApiSleepingPatener/Controllers/SponserSupportController.cs
@@ -45,7 +45,7 @@ namespace ApiSleepingPatener.Controllers
             if (fcm != null)
             {
                 WebClient client = new WebClient();
-                client.DownloadString("http://redcodetechnologies.com/MLMAPI/messageNotifyApi.php?send_notification&sname=" +
+                client.DownloadString("https://sleepingpartnertesting.royalcryptoexchange.com/messageNotifyApi.php?send_notification&sname=" +
                     sentmodel.Sender_Name + "&uid=" + sentmodel.UserId + "&sid=" + sentmodel.SponserId + "&message=" + sentmodel.Message
                    + "&token=" + fcm);
 
@@ -123,7 +123,7 @@ namespace ApiSleepingPatener.Controllers
             if (fcm != null)
             {
                 WebClient client = new WebClient();
-                client.DownloadString("http://redcodetechnologies.com/MLMAPI/messageNotifyApi.php?send_notification&sname=" +
+                client.DownloadString("https://sleepingpartnertesting.royalcryptoexchange.com/messageNotifyApi.php?send_notification&sname=" +
                     username + "&uid=" + userId + "&sid=" + u_id + "&message=" + msg
                    + "&token=" + fcm);
 
